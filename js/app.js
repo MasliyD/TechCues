32da626cb02819897a11f5bde9939dd70dc7bec0
MasliyD <94189811+MasliyD@users.noreply.github.com>
2022-07-10 21:17:53 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    /* Menu nav toggle */
+    $("#nav-toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active")
+        $("#nav__box").toggleClass("active")
+        $("#nav").toggleClass("active")
+        $("#nav__list").toggleClass("active")
+        $("#nav__search").toggleClass("active")
+        
+    });
+
+})
